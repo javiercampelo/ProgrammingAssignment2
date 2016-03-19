b76e6d1178a2ed7e358def072277677663ee1989
javiercampelo <javier.campelo@gmail.com>
2016-03-19 19:01:55 +0100

    Se crean las funciones

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# makeCacheMatrix crea una lista que contiene:
+# 1. establecer el valor de la matriz
+# 2. obtener el valor de la matriz
+# 3. establece el valor de inversa de la matriz
+# 4. obtener el valor de la inversa de la matriz
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+# La siguiente funcion devuelde la inversa de la matriz, comprobando previamente 
+# se ha calculado previamente. Si se ha calculado, devuelve el resultado sin realizar 
+# el calculo. En caso contrario, lo calcula.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("El dato está cacheado")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
